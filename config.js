fb46ff1aebe1bbc47b65380013895a3ec7a2f2b0
Jayzhoujs <575572498@qq.com>
2018-06-20 17:10:09 +0800

    init

---
+++ config.js
@@ -0,0 +1,37 @@
+export default {
+	secret: '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+	mongo: {
+		development: {
+			connectionString: 'mongodb://127.0.0.1:27017/mall'
+		},
+		production: {
+			connectionString: 'mongodb://127.0.0.1:27017/mall'
+		}
+	},
+	redis: {
+		development: {
+			connectionString: 'redis://127.0.0.1:6379'
+		},
+		production: {
+			connectionString: 'redis://127.0.0.1:6379',
+		}
+	},
+	upload: {
+		tmp : 'public/tmp/',
+		path: 'public/uploads/'
+	},
+	superAdmin: {
+		username: 'admin', 
+		password: '123456', 
+	},
+	orderStatus: {
+		'submitted': '已提交', 
+		'canceled' : '已取消', 
+		'confirmed': '已确认', 
+		'finished' : '已完成', 
+	},
+	wechat: {
+		appid: 'wxb89e07608fa7325c',
+		secret: '9be58f5cc497139f65653927b6ebccbe',
+	},
+}
